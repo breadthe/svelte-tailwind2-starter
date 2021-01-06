ef2a2cb71875fcb3cff3e8dfa4fe62adfb86aaf6
breadthe <omigoshdev@protonmail.com>
2021-01-06 00:18:56 -0600

    Purge styles in production only + add dynamic classes to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+const production = !process.env.ROLLUP_WATCH;
+
 module.exports = {
-    purge: [
-        './src/**/*.html',
-        './src/**/*.svelte',
-    ],
+    purge: {
+        enabled: production,
+        content: [
+            './src/**/*.html',
+            './src/**/*.svelte',
+        ],
+        options: {
+            safelist: [
+                'border-blue-300',
+                'border-orange-500',
+                'border-pink-100',
+                'border-pink-900',
+                'dark',
+                'from-blue-500',
+                'from-blue-700',
+                'from-yellow-200',
+                'text-pink-100',
+                'text-pink-900',
+                'to-blue-800',
+                'to-pink-300',
+                'to-purple-800',
+                'to-yellow-500',
+            ],
+        }
+    },
     darkMode: 'class', // 'media' / 'class' / 'false'
     theme: {
         colors: {
