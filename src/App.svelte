946568a4bc95207f7d191c713f79a23dee986665
breadthe <omigoshdev@protonmail.com>
2021-01-05 20:40:54 -0600

    Random surprise

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,43 @@
 
   import AppearanceToggler from "./AppearanceToggler.svelte";
 
-  export let name;
+  export let title;
+
+  const colors = [
+    "gray",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "teal",
+    "blue",
+    "indigo",
+    "purple",
+    "pink"
+  ];
+  const shades = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+  let color = randomColor();
+  let shade = randomShade();
+
+  function clickMe() {
+    color = randomColor();
+    shade = randomShade();
+  }
+
+  function randomColor() {
+    return colors[randomRange(0, colors.length - 1)];
+  }
+
+  function randomShade() {
+    return shades[randomRange(0, shades.length - 1)];
+  }
+
+  function randomRange(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 </script>
 
 <style>
@@ -31,7 +67,7 @@
   class:to-pink-300={!$dark}>
   <div class="flex flex-col items-center justify-center min-h-screen space-y-8">
     <div class:text-pink-100={$dark} class:text-pink-900={!$dark}>
-      <h1 class="hello leading-tight">Hello {name}!</h1>
+      <h1 class="hello leading-tight">{title}</h1>
       Visit the
       <a
         href="https://svelte.dev/tutorial"
@@ -46,7 +82,8 @@
     <div class="flex items-center">
       <AppearanceToggler />
     </div>
-    <div class="space-y-2">
+    <div
+      class="border-4 border-dashed max-w-sm p-4 rounded-lg space-y-2 {`border-${color}-${shade}`}">
       <p class:text-pink-100={$dark} class:text-pink-900={!$dark}>
         This next button demonstrates the use of
         <span class="highlight">@apply</span>
@@ -54,7 +91,9 @@
         <span class="highlight">global.css</span>
         for examples.
       </p>
-      <button type="button" class="custom-btn">Custom Button</button>
+      <button type="button" class="custom-btn" on:click={() => clickMe()}>
+        Click Me
+      </button>
     </div>
   </div>
 </main>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import App from './App.svelte';
 import global from "../public/global.css";
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+    target: document.body,
+    props: {
+        title: 'Svelte + Tailwind 2'
+    }
 });
 
 export default app;
